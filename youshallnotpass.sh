ff74f66c3f59fb92a2c34c2023833ad9618782ee
Linkinverse <link.inverse@gmail.com>
2020-05-01 03:42:18 +0200

    Add files via upload

---
+++ youshallnotpass.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# THIS IS TO ALLOW SSH, NO REQUESTS LIMITS PER MONTH AS DYNAMIC DNS, ADD TO CRON TO BE EXECUTED AS MANY TIMES YOU WANT
+
+#please note you have to specify full path for this to work from crontab
+LOGSIZ=$( stat -c %s /var/log/youshallnotpass.log)
+if (( $LOGSIZ > 50000 )); then
+    echo >/var/log/youshallnotpass.log
+    echo Log too big, wiping it.
+fi
+
+#save datetime to logs
+date
+
+# ADD YOUR CONFIGURATION BELOW
+# please avoid using same filename ip.txt since the filename is the identifier for the device
+
+echo ---- WHITELIST IP ALLOW ADMIN ----
+./youshallnotpass-firewall-update.sh https://yourwebsite.com/myip/ip.txt
+./youshallnotpass-firewall-update.sh http://freewebhosting.com/backupaccess/johnsmith.txt
